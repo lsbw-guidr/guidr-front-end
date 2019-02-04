488a4c31e3b8deef2b61e8fac1433dd4a7cc70cb
Zooheck <davidscottflack@gmail.com>
2019-02-03 21:37:19 -0600

    Add SCSS and begin welcome page

--- guidr/src/App.js
+++ guidr/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './Main-Styles/App.scss';
+import WelcomePage from './components/WelcomePage/WelcomePage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         
-
+        <WelcomePage />
       </div>
     );
   }
---
+++ guidr/src/components/WelcomePage/WelcomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './_welcome-page.scss'
+const WelcomePage = () => {
+  return (
+    <div className="welcome-page">
+      <div className="header-container">
+        <i className="fas fa-map-marked-alt fa-5x"></i>
+        <h1>Guidr</h1>
+      </div>  
+      <h2>Your adventure awaits.</h2>
+    </div>
+  )
+}
+
+export default WelcomePage
