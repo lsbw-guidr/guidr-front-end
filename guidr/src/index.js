ba5261f6e13234a2cd43e163ec03a6b480318521
Zooheck <davidscottflack@gmail.com>
2019-02-03 19:07:03 -0600

    Remove CRA defaults. Setup basic redux boilerplate. Wrap App in react router

--- guidr/src/index.js
+++ guidr/src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// ===================== REDUX IMPORTS =========================
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
+// ===================== REACT ROUTER ==========================
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, logger)
+  );
+
+ReactDOM.render(
+<Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+</Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
