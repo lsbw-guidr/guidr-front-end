e95916f91cb397403838f628d170bf8ac8c6411b
Zooheck <davidscottflack@gmail.com>
2019-02-07 12:19:08 -0600

    Add edit user icon

--- guidr/src/components/UserProfile/UserProfile.js
+++ guidr/src/components/UserProfile/UserProfile.js
@@ -72,7 +72,7 @@ class UserProfile extends Component {
           </div>
 
           
-          <button onClick={this.toggleUpdate}>Update Info</button>
+          <button onClick={this.toggleUpdate}><i class="fas fa-edit"></i></button>
         </div>
         {/* NAV LINKS, ONE TO THE TRIP LIST (DEFAULT) AND THE OTHER TO ADD TRIP FORM  */}
         <div className="link-container">
