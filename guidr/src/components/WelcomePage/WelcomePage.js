645bf769abe31d555ab9ac66c2da88e8b657767d
James Basile <jbasile14@gmail.com>
2019-02-07 20:49:13 -0500

    styled the login page

--- guidr/src/components/WelcomePage/WelcomePage.js
+++ guidr/src/components/WelcomePage/WelcomePage.js
@@ -13,20 +13,20 @@ class WelcomePage extends React.Component {
   }
   render() {
     return (
+
       <div className="welcome-page">
         <div className="welcome-container">
           <div className="header-container">
 
-            {/* <i className="fas fa-map-marked-alt fa-5x"></i>
-        <h1>guidr</h1> */}
             <img alt="guidr" src={require('../../assets/logo_white.png')} />
           </div>
-        
-        <h2>Your adventure awaits...</h2>
-        <button className="button register" onClick={this.goToRegisterPage}>Register</button>
-        <button className="button log-in" onClick={this.goToLogInPage}>Log In</button>
+
+          <h2>Your adventure awaits...</h2>
+          <button className="button register" onClick={this.goToRegisterPage}>Register</button>
+          <button className="button log-in" onClick={this.goToLogInPage}>Log In</button>
+        </div>
       </div>
-      </div>
+
     )
   }
 }
