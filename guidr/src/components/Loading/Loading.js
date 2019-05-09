6d8af701c6356cd8a195b5f506610d193ece3c3e
Zooheck <davidscottflack@gmail.com>
2019-05-08 20:43:27 -0500

    Center loading wheel

--- guidr/src/components/Loading/Loading.js
+++ guidr/src/components/Loading/Loading.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Loader from "react-loader-spinner";
+
+import "./_loading.scss";
 export const Loading = () => {
   return (
-    <div>
+    <div className="main-loading">
       <Loader type="Oval" color="#00BFFF" height={80} width={80} />
     </div>
   );
